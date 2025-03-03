aec6bc7694f632940a789610df98ececb08bd415
Jose Alisson Tavares de Oliveira <alisson.dev737@gmail.com>
2025-03-03 18:19:56 -0300

    3o commit

--- Lunar_Bank.py
+++ Lunar_Bank.py
@@ -48,37 +48,51 @@ while True:
 
         else:
             if option == '3':
-                deposit_value=int(input('Whats deposit value?\n'))
-                balance+=deposit_value
-                transactions.append(f'Deposit: R$ + {deposit_value:.2f}\n')
-                transactions.append(f'{"*":>25}{balance:<25.2f}')
-                print(f'You new balance is: R${balance:.2f}', end='')
-                print()
-                menu()
+                while True:
+                    try:
+                        deposit_value=int(input('Whats deposit value?\n'))
+                        if deposit_value<=0:
+                            print('The value must be great than 0,00. Try again.')
+                            print()
+                        else:
+                            if deposit_value >= 0:
+                                balance+=deposit_value
+                                transactions.append(f'Deposit: R$ + {deposit_value:.2f}\n')
+                                transactions.append(f'{"*":>25}{balance:<25.2f}')
+                                print(f'You new balance is: R${balance:.2f}. Thank you for to use our service.', end='')
+                                print() 
+                                menu()
+                    except ValueError:
+                        print('Erro: Enter a valid value.')
+                        print()
 
             else:
                 if option == '4':
-                    withdraw_value=int(input('Whats withdraw value?\n'))
-                    print()
-                    if withdraw_value > 0 and withdraw_value <= balance and withdraw_value <= dayly_limit:
-                        balance -= withdraw_value
-                        dayly_limit-= withdraw_value
-                        transactions.append(f'Withdraw: R$ - {withdraw_value}\n')
-                        transactions.append(f'{'*':>25}{balance:<.2f}')
-                        print('Withdrawal successful.')
-                        print(f'You atual balance is: {balance:.2f}')
-                    else:
-                        if withdraw_value<0:
-                            print(f'Enter a value greater than R$1,00.')
-                        else:
-                            if withdraw_value>balance:
-                                print(f'Insufficient balance for this withdraw.')
+                    while True:
+                        try: 
+                            withdraw_value=int(input('Whats withdraw value?\n'))
+                            print()
+                            if withdraw_value > 0 and withdraw_value <= balance and withdraw_value <= dayly_limit:
+                                    balance -= withdraw_value
+                                    dayly_limit-= withdraw_value
+                                    transactions.append(f'Withdraw: R$ - {withdraw_value}\n')
+                                    transactions.append(f'{'*':>25}{balance:<.2f}')
+                                    print('Withdrawal successful.')
+                                    print(f'You atual balance is: {balance:.2f}')
                             else:
-                                if withdraw_value>dayly_limit:
-                                    print(f'Unauthorized withdrawal. Your remaining daily limit is: R${dayly_limit}')
-                                print()
-                    menu()
-
+                                if withdraw_value<0:
+                                    print(f'Enter a value greater than R$1,00.')
+                                else:
+                                    if withdraw_value>balance:
+                                        print(f'Insufficient balance for this withdraw.')
+                                    else:
+                                        if withdraw_value>dayly_limit:
+                                            print(f'Unauthorized withdrawal. Your remaining daily limit is: R${dayly_limit}')
+                                            print()
+                                            menu()
+                        except ValueError:
+                            print('Erro. Enter a valid value.')
+                            print()
                 else:
                     if option == '5':
                         print('Thankyou for use our system. Leaving...')
